b91dbe33b5a5f7bb4d94cf2974e1f8a489fcff77
Quenton Barger <qbarger22@gmail.com>
2024-10-13 22:46:43 -0600

    fibonacciNumberFinder added

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -39,7 +39,20 @@ class Calculator {
     etc
      */
     int fibonacciNumberFinder(int n){
-        return 0;
+        if (n <= 1) {
+            return n;
+        }
+
+        int a = 0, b = 1;
+        int result = 0;
+
+        for (int i = 2; i <= n; i++) {
+            result = a + b;
+            a = b;
+            b = result;
+        }
+
+        return result;
     }
 
 
